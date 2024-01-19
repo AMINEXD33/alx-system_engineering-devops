f9c67bf7c9b0142c554a86f82bcc7420d7ef66d9
AMINEXD33 <amine.gamer222@gmail.com>
2024-01-19 17:48:16 +0100

    WOOOOOORK XD

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,7 @@
 # Install flask version 2.1.0
 
 $base_package = 'flask'
-package { $base_package
+package { $base_package:
 ensure => '2.1.0',
-name   => $base_package,
-provider => 'pip3'
+provider => 'pip3
 }
