6c6dbb8f46b7f6860c27647909e64236a7de6645
AMINEXD33 <amine.gamer222@gmail.com>
2024-01-21 19:30:09 +0100

    final

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,10 @@
-'Host *'                 => {
-  'passwordAuthentification'              => 'no',
-  'IdentityFile'       => '~/.ssh/school',
+file_line{'Turn off passwd auth':
+path => '/etc/ssh/ssh_config',
+line => 'PasswordAuthentication no'
+}
+
+file_line{'Declare identity file':
+path => '/etc/ssh/ssh_config',
+line => 'IdentityFile ~/.ssh/school'
+
 }
