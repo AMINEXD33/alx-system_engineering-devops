18ef47784cb6bb6f293d54893a0a6fecd7da5d6b
AMINEXD33 <amine.gamer222@gmail.com>
2024-01-21 19:35:23 +0100

    final

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,6 @@
+# configuration to use a specific public key, plus 
+# not to try to auth using a password
+
 file_line{'Turn off passwd auth':
 path => '/etc/ssh/ssh_config',
 line => 'PasswordAuthentication no'
