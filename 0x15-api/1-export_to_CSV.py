dab470f592e9be3a7a7a0ac8d4db79c7c72e2270
AMINEXD33 <amine.gamer222@gmail.com>
2024-02-19 19:05:20 +0100

    task 2 (user id file name )

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -37,7 +37,7 @@ def driver():
     except Exception:
         return
     todos, user = get_user_data(argv[1])
-    with open('{id}.csv', 'w', newline='') as csvfile:
+    with open(f'{id}.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile,
                             delimiter=',',
                             quotechar='|',
